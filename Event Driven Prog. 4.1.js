c93be803415eae40eb9df918bd102d0c38269bce
Marina De Castro <132936941+cyn1calmars@users.noreply.github.com>
2023-08-14 19:13:44 -0700

    Add files via upload

---
+++ Event Driven Prog. 4.1.js
@@ -0,0 +1,11 @@
+
+function start(){
+myTimeout = setTimeout(start, 1000);
+let x = document.getElementById('memeimage');
+x.style.marginTop = x.style.marginTop == "0px" ? "300px" : "0px";
+
+}
+
+function stop(){
+    clearTimeout(myTimeout);
+}
